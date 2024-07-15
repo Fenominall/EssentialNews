cf8ded74363dba218886b1547855c084ed466735
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-15 19:41:42 +0300

    Added LocalFeedLoader that will perform local operations with saving, loading, validating and deleting the cache

--- EssentialNews/EssentialNews/FeedCache/LocalFeedLoader.swift
+++ EssentialNews/EssentialNews/FeedCache/LocalFeedLoader.swift
@@ -10,3 +10,33 @@ import Foundation
 public final class LocalFeedLoader {
     
 }
+
+extension LocalFeedLoader: FeedLoader {
+    private final class FeedLoaderTaskWrapper: FeedLoaderTask {
+        private var completion: ((RemoteFeedLoader.LoadResult) -> Void)?
+                
+        init(_ completion: @escaping (RemoteFeedLoader.LoadResult) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: RemoteFeedLoader.LoadResult) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherCompletions()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+    
+    public func load(
+        completion: @escaping (LoadFeedResult) -> Void
+    ) -> any FeedLoaderTask {
+        let task = FeedLoaderTaskWrapper(completion)
+        
+        return task
+    }
+}
