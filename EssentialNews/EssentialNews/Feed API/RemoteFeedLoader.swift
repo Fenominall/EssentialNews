5775fbd9013319f9d8f902caf3b3c0a20dd06ffc
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-20 17:02:47 +0300

    REFACTOR: 'RemoteFeedLoader' is now using generic 'RemoteLoader' to remove the duplication

--- EssentialNews/EssentialNews/Feed API/RemoteFeedLoader.swift
+++ EssentialNews/EssentialNews/Feed API/RemoteFeedLoader.swift
@@ -8,66 +8,15 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-    private let url: URL
-    private let client: HTTPClient
+    private let loader: RemoteLoader<[Article], FeedItemsMapper>
     
-    public init(
-        url: URL,
-        client: HTTPClient
-    ) {
-        self.url = url
-        self.client = client
+    public init(loader: RemoteLoader<[Article], FeedItemsMapper>) {
+        self.loader = loader
     }
-    
-    public typealias LoadResult = FeedLoader.LoadFeedResult
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    private final class HTTPClientTaskWrapper: FeedLoaderTask {
-        private var completion: ((RemoteFeedLoader.LoadResult) -> Void)?
         
-        var wrapped: HTTPClientTask?
-        
-        init(_ completion: @escaping (RemoteFeedLoader.LoadResult) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: RemoteFeedLoader.LoadResult) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
     @discardableResult
-    public func load(completion: @escaping (LoadResult) -> Void) -> FeedLoaderTask {
-        let task = HTTPClientTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            do {
-                switch result {
-                case let .success((data, response)):
-                    let articles = try FeedItemsMapper.map(data, from: response)
-                    completion(.success(articles))
-                case .failure:
-                    completion(.failure(Error.connectivity))
-                }
-            } catch {
-                completion(.failure(Error.invalidData))
-            }
-        }
-        return task
+    public func load(completion: @escaping (LoadFeedResult) -> Void) -> any FeedLoaderTask {
+        return loader.load(completion: completion)
     }
 }
 
