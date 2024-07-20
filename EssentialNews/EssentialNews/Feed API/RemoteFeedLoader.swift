1db58fcabced0d38a4c4c3434f88f6fbcc03cba4
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-20 17:58:57 +0300

    Reverted back the logic in the 'RemoteFeedLoader'

--- EssentialNews/EssentialNews/Feed API/RemoteFeedLoader.swift
+++ EssentialNews/EssentialNews/Feed API/RemoteFeedLoader.swift
@@ -8,15 +8,42 @@
 import Foundation
 
 public final class RemoteFeedLoader: FeedLoader {
-    private let loader: RemoteLoader<[Article], FeedItemsMapper>
+    private let url: URL
+    private let client: HTTPClient
     
-    public init(loader: RemoteLoader<[Article], FeedItemsMapper>) {
-        self.loader = loader
+    public init(
+        url: URL,
+        client: HTTPClient
+    ) {
+        self.url = url
+        self.client = client
     }
-        
+    
+    public typealias LoadResult = FeedLoader.LoadFeedResult
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
     @discardableResult
-    public func load(completion: @escaping (LoadFeedResult) -> Void) -> any FeedLoaderTask {
-        return loader.load(completion: completion)
+    public func load(completion: @escaping (LoadResult) -> Void) -> FeedLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            do {
+                switch result {
+                case let .success((data, response)):
+                    let articles = try FeedItemsMapper.map(data, from: response)
+                    completion(.success(articles))
+                case .failure:
+                    completion(.failure(Error.connectivity))
+                }
+            } catch {
+                completion(.failure(Error.invalidData))
+            }
+        }
+        return task
     }
 }
-
