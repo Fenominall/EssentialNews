c4523694a439ecbffcf5ec70f5afb8807fdbd85f
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-21 18:43:00 +0300

    Added find method to the ManagedCache that fetches and return the instance of the ManagedCache The find method is used to deleteCache where we find the Managed in the context than delete it and saving the context Did Add newUniqueInstance - its used to override previously inserted cache

--- EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/ManagedCache+CoreDataProperties.swift
+++ EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/ManagedCache+CoreDataProperties.swift
@@ -20,4 +20,21 @@ extension ManagedCache {
     var localFeed: [LocalArticle] {
         return feed.compactMap { ($0 as? ManagedArticle)?.local }
     }
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    static func deleteCache(in context: NSManagedObjectContext) throws {
+        try find(in: context)
+            .map(context.delete)
+            .map(context.save)
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        try deleteCache(in: context)
+        return ManagedCache(context: context)
+    }
 }
