96a6352c7fbca65c06fa84798ec117581f63410a
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-21 21:54:33 +0300

    Did implement FeedStore and provided the performing logic for retrieving, inserting and deleting the LocalArticle models

--- EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -10,15 +10,38 @@ import Foundation
 extension CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        
+        performAsync { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map {
+                    CachedFeed(
+                        feed: $0.localFeed,
+                        timestamp: $0.timestamp)
+                }
+            })
+        }
+    }
+    
+    public func insert(
+        _ articles: [LocalArticle],
+        timestamp: Date,
+        completion: @escaping (InsertionCompletion) -> Void
+    ) {
+        performAsync { context in
+            completion(Result {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache?.timestamp = timestamp
+                managedCache?.feed = ManagedArticle.articles(from: articles, in: context)
+                try context.save()
+            })
+        }
     }
     
     public func deleteCachedFeed(completion: @escaping (DeletionCompletion) -> Void) {
-        
+        performAsync { context in
+            completion(Result {
+                try ManagedCache
+                    .deleteCache(in: context)
+            })
+        }
     }
-    
-    public func insert(_ articles: [LocalArticle], timestamp: Date, completion: @escaping (InsertionCompletion) -> Void) {
-        
-    }
-    
 }
