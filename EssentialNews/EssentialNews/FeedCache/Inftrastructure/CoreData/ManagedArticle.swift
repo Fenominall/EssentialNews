0dd7787ffa948ef998a7b2be3438de510c88f911
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-21 19:07:38 +0300

    Did add the logic to the ManagedArticle to create and fetch managedArticles Method first returns an instance of an optional ManagedArticle
    Method articles  creates an ordered set of ManagedArticles that will be stored in the ManagedCache
    Overriding prepareForDeletion to temporarily store data of the images before its deletion

--- EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/ManagedArticle.swift
+++ EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/ManagedArticle.swift
@@ -40,4 +40,54 @@ extension ManagedArticle {
             content: content
         )
     }
+    
+    static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedArticle? {
+        let request = NSFetchRequest<ManagedArticle>(entityName: entity().name!)
+        request.predicate = NSPredicate(
+            format: "%K = %@",
+            argumentArray: [#keyPath(ManagedArticle.url), url]
+        )
+        request.returnsObjectsAsFaults = false
+        request.fetchLimit = 1
+        return try context.fetch(request).first
+    }
+    
+    static func articles(from localFeed: [LocalArticle], in context: NSManagedObjectContext) -> NSOrderedSet {
+        let articles = NSOrderedSet(array: localFeed.map { local in
+            let source = ManagedSource(context: context)
+            source.id = local.source.id
+            source.name = local.source.name
+            
+            let managed = ManagedArticle(context: context)
+            managed.source = source
+            managed.author = local.author
+            managed.title = local.title
+            managed.desscription = local.description
+            managed.url = local.url
+            managed.urlToImage = local.urlToImage
+            managed.publishedAt = local.publishedAt
+            managed.content = local.content
+            
+            guard let urlToImage = local.urlToImage else {
+                managed.data = nil
+                return managed
+            }
+            managed.data = context.userInfo[urlToImage] as? Data
+            
+            return managed
+        })
+        
+        context.userInfo.removeAllObjects()
+        return articles
+    }
+    
+    // Called for each CoreData entity before it`s deletion
+    public override func prepareForDeletion() {
+        super.prepareForDeletion()
+        // storing the data for url
+        guard let unwrappedUrlToImage = urlToImage else {
+            return
+        }
+        managedObjectContext?.userInfo[unwrappedUrlToImage] = data
+    }
 }
