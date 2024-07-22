d6d394742ad674cd771556907ba6c4e800410301
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-22 19:00:36 +0300

    - Added 'FeedArticleCellController' as NSObject that will be creating a cellController for each cell -Using 'FeedArticlesCellControllerDelegate' for requesting the image and canceling the image request - Creating a cell in the cellForRowAt - Using delegate to request the image load on the cell creation as well as when onRetry closure call if the load fails - canceling the load and deinitializing the cell on cancelPrefetchingForRowsAt and didEndDisplaying cell - Conformed and implement the methods of the LoadResourcePresenter which are:     - ResourceView to set the received image for the cell image view     - ResourceErrorViewModel shows or hides the retry button based on the error message if its nil or not     - ResourceLoadingViewModel is making the shimmering animation on the articlesImageContainer while the image is loading

--- EssentialNews/EssentialNewsiOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialNews/EssentialNewsiOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -5,4 +5,86 @@
 //  Created by Fenominall on 7/22/24.
 //
 
-import Foundation
+import UIKit
+import EssentialNews
+
+public protocol FeedArticlesCellControllerDelegate {
+    func didRequestImage()
+    func didCancelImageRequest()
+}
+
+public final class FeedArticleCellController: NSObject {
+    
+    public typealias ResourceViewModel = UIImage
+    
+    private let viewModel: FeedArticleViewModel
+    private let delegate: FeedArticlesCellControllerDelegate
+    private let selection: () -> Void
+    private var cell: FeedArticleViewCell?
+    
+    init(
+        viewModel: FeedArticleViewModel,
+        delegate: FeedArticlesCellControllerDelegate,
+        selection: @escaping () -> Void
+    ) {
+        self.viewModel = viewModel
+        self.delegate = delegate
+        self.selection = selection
+    }
+}
+
+extension FeedArticleCellController: UITableViewDataSource, UITableViewDelegate, UITableViewDataSourcePrefetching {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell = tableView.dequeueReusableCell()
+        cell?.descriptionLabel.text = viewModel.title
+        cell?.onRetry = { [weak self] in
+            self?.delegate.didRequestImage()
+        }
+        delegate.didRequestImage()
+        return cell ?? UITableViewCell()
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selection()
+    }
+    
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cancelLoad()
+    }
+    
+    public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        delegate.didRequestImage()
+    }
+    
+    public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        cancelLoad()
+    }
+    
+    private  func cancelLoad() {
+        releaseCellForReuse()
+        delegate.didCancelImageRequest()
+    }
+    
+    private func releaseCellForReuse() {
+        cell = nil
+    }
+}
+
+extension FeedArticleCellController: ResourceView, ResourceErrorView, ResourceLoadingView {
+    
+    public func display(_ viewModel: UIImage) {
+        cell?.articleImageView.setImageAnimated(viewModel)
+    }
+    
+    public func display(_ viewModel: EssentialNews.ResourceLoadingViewModel) {
+        cell?.articlesImageContainer.isShimmering = viewModel.isLoading
+    }
+    
+    public func display(_ viewModel: EssentialNews.ResourceErrorViewModel) {
+        cell?.retryButton.isHidden = viewModel.message == nil
+    }
+}
