fd756b8e8f91fb9318870de5b390aaa7e4cb64a9
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-22 14:15:35 +0300

    Added Feed Presentation group

---
+++ EssentialNews/EssentialNewsiOS/CellController.swift
@@ -0,0 +1,8 @@
+//
+//  CellController.swift
+//  EssentialNewsiOS
+//
+//  Created by Fenominall on 7/22/24.
+//
+
+import Foundation
---
+++ EssentialNews/EssentialNewsiOS/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  EssentialNewsiOS
+//
+//  Created by Fenominall on 7/22/24.
+//
+
+import Foundation
+import UIKit
+
+public final class ListViewController: UITableViewController {
+    
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
