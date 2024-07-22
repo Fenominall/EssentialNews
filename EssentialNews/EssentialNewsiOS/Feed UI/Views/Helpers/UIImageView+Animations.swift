2f5edb8bc3de8e217408f105271d5840e3875d6a
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-22 18:54:59 +0300

    Added UIImageView extension that sets the image smoothly with the animation

--- EssentialNews/EssentialNewsiOS/Feed UI/Views/Helpers/UIImageView+Animations.swift
+++ EssentialNews/EssentialNewsiOS/Feed UI/Views/Helpers/UIImageView+Animations.swift
@@ -5,4 +5,18 @@
 //  Created by Fenominall on 7/22/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
+
