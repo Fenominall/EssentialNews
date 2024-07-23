c20ca48711e01e56ad5890b9a520750b75f57a13
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-23 21:28:46 +0300

    Made implementation of the 'FeedImageDataStore' in the CoreData to asynchronously insert and retrieve data in the CoreData ManagedCache

--- EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
+++ EssentialNews/EssentialNews/FeedCache/Inftrastructure/CoreData/CoreDataFeedStore+FeedImageDataStore.swift
@@ -5,4 +5,25 @@
 //  Created by Fenominall on 7/23/24.
 //
 
+import CoreData
 import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    public func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+        performAsync { context in
+            completion(Result {
+                try ManagedArticle.first(with: url, in: context)
+                    .map { $0.data = data }
+                    .map(context.save)
+            })
+        }
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        performAsync { context in
+            completion(Result {
+                try ManagedArticle.data(with: url, in: context)
+            })
+        }
+    }
+}
