5ee8981230049b30b861091b921919ded0f3d7e0
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-23 20:08:35 +0300

    Created FeedUIComposer for composing all the necessary objects to make the application work

--- EssentialNewsApp/EssentialNewsApp/FeedUIComposer.swift
+++ EssentialNewsApp/EssentialNewsApp/FeedUIComposer.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import EssentialNews
+import EssentialNewsiOS
+
+final class FeedUIComposer {
+    private init() {}
+    
+    static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> ListViewController {
+        let feedViewController = ListViewController()
+        
+        return feedViewController
+    }
+}
+
+
+final class FeedViewAdapter: ResourceView {
+    private weak var controller: ListViewController?
+    private let imageLoader: FeedImageDataLoader
+    
+    init(
+        controller: ListViewController? = nil,
+        imageLoader: FeedImageDataLoader
+    ) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        
+    }
+}
