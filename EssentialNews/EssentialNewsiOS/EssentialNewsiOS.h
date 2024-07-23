9383c3233b2fc325d75e22e68bc2bf5af3ec427c
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-23 21:52:09 +0300

    Fixed the warnings with the Defined Modules

---
+++ EssentialNews/EssentialNewsiOS/EssentialNewsiOS.h
@@ -0,0 +1,18 @@
+//
+//  EssentialNewsiOS.h
+//  EssentialNewsiOS
+//
+//  Created by Fenominall on 7/21/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialNewsiOS.
+FOUNDATION_EXPORT double EssentialNewsiOSVersionNumber;
+
+//! Project version string for EssentialNewsiOS.
+FOUNDATION_EXPORT const unsigned char EssentialNewsiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialNewsiOS/PublicHeader.h>
+
+
