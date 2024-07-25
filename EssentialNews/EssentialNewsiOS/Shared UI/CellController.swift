9d5ff651702bdbd762794d424841a83dc4cb8dba
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-25 17:43:57 +0300

    Added extra space for better separation

--- EssentialNews/EssentialNewsiOS/Shared UI/CellController.swift
+++ EssentialNews/EssentialNewsiOS/Shared UI/CellController.swift
@@ -26,6 +26,7 @@ extension CellController: Equatable {
         lhs.id == rhs.id
     }
 }
+
 extension CellController: Hashable {
     public func hash(into hasher: inout Hasher) {
         hasher.combine(id)
