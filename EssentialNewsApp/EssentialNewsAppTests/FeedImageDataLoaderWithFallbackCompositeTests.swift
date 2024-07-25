b225e095b58175f1c5c7c30828af3df76fc4221c
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-25 22:44:28 +0300

    'FeedImageDataLoaderWithFallbackComposite' .loadImageData delivers fallback data on fallback loader success

--- EssentialNewsApp/EssentialNewsAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialNewsApp/EssentialNewsAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -35,7 +35,10 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
             case .success:
                 completion(result)
             case .failure:
-                task.wrapped = self?.fallback.loadImageData(from: url) { _ in }
+                task.wrapped = self?.fallback.loadImageData(
+                    from: url,
+                    completion: completion
+                )
             }
         }
         return task
@@ -92,6 +95,16 @@ class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         expect(sut, toCompleteWith: .success(primaryData), when: {
             primaryLoader.complete(with: primaryData)
         })
+    }
+    
+    func test_loadImageData_deliversFallbackDataOnFallbackLoaderSuccess() {
+        let fallbackData = anyData()
+        let (sut, primaryLoader, fallbackLoader) = makeSUT()
+        
+        expect(sut, toCompleteWith: .success(fallbackData)) {
+            primaryLoader.complete(with: anyNSError())
+            fallbackLoader.complete(with: fallbackData)
+        }
     }
     
     // MARK: - Helpers
