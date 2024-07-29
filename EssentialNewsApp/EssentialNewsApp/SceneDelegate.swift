103a29568c8648c08f92971eaf88d3dea7a24f83
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-29 11:00:46 +0300

    Add #if DEBUG compilation directive to prevent test-specific code from being deployed in production

--- EssentialNewsApp/EssentialNewsApp/SceneDelegate.swift
+++ EssentialNewsApp/EssentialNewsApp/SceneDelegate.swift
@@ -82,13 +82,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     // MARK: - Helpers
     private func makeRemoteClient() -> HTTPClient {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline":
+        #if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
             return AlwaysFailingHTTPClient()
-            
-        default:
-            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         }
+        #endif
+            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
 }
 
