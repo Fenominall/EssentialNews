759dcdaacbe8c754f709dd95c58239cc1203c2a9
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-30 22:27:15 +0300

    Removed unused code and empty spaced Controlling the loading state with isLoading bool

--- EssentialNewsApp/EssentialNewsApp/Feed UI Composer/FeedImageDataLoaderPresentationAdapter.swift
+++ EssentialNewsApp/EssentialNewsApp/Feed UI Composer/FeedImageDataLoaderPresentationAdapter.swift
@@ -8,10 +8,11 @@
 import EssentialNews
 import EssentialNewsiOS
 
-final class FeedImageDataLoaderPresentationAdapter<Resource, View: ResourceView>: FeedArticlesCellControllerDelegate where View.ResourceViewModel == Resource {
+final class FeedImageDataLoaderPresentationAdapter<Resource, View: ResourceView>: FeedArticlesCellControllerDelegate {
     private let model: Article
     private let imageLoader: FeedImageDataLoader
     private var task: FeedImageDataLoaderTask?
+    private var isLoading = false
     
     var presenter: LoadResourcePresenter<Data, View>?
     
@@ -21,17 +22,25 @@ final class FeedImageDataLoaderPresentationAdapter<Resource, View: ResourceView>
     }
     
     func didRequestImage() {
+        guard !isLoading else { return }
+
         presenter?.didStartLoading()
+        isLoading = true
         
-        guard let imageURL = model.urlToImage else { return }
+        guard let imageURL = model.urlToImage else {
+            isLoading = false
+            return
+        }
+        
         task = imageLoader.loadImageData(from: imageURL) { [weak self] result in
+            guard let self = self else { return }
             switch result {
             case let .success(data):
-                self?.presenter?.didFinishLoading(with: data)
-                
+                self.presenter?.didFinishLoading(with: data)
             case let .failure(error):
-                self?.presenter?.didFinishLoading(with: error)
+                self.presenter?.didFinishLoading(with: error)
             }
+            self.isLoading = false
         }
     }
     
