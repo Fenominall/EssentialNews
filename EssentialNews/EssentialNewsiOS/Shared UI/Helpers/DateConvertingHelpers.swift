c201a928ef5127a34638d6e71900097b3f28361d
Vladislav Todorov <it.vtodorov@gmail.com>
2024-07-30 22:23:05 +0300

    Added date converting methods to represent the date in the readble format

--- EssentialNews/EssentialNewsiOS/Shared UI/Helpers/DateConvertingHelpers.swift
+++ EssentialNews/EssentialNewsiOS/Shared UI/Helpers/DateConvertingHelpers.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+func convertDate(with date: Date?) -> String {
+    guard let validDate = date else {
+        return "Unknown Date"
+    }
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    dateFormatter.timeStyle = .short
+    return dateFormatter.string(from: validDate)
+}
+
+func convertDateForShort(with date: Date) -> String {
+    let calendar = Calendar.current
+    if calendar.isDateInToday(date) {
+        return "Today"
+    } else {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE, dd MMM"
+        return dateFormatter.string(from: date)
+    }
+}
