374594bc019a9a220ac111da52d3c2ddc81915db
Vladislav Todorov <it.vtodorov@gmail.com>
2024-08-03 16:12:03 +0300

    Created 'FeedArticleDetailsViewModel' that will be used by 'FeedArticleDetailsViewController' to populate the data in the controller

--- EssentialNews/EssentialNewsiOS/Article details UI/FeedArticleDetailsViewModel.swift
+++ EssentialNews/EssentialNewsiOS/Article details UI/FeedArticleDetailsViewModel.swift
@@ -5,4 +5,32 @@
 //  Created by Fenominall on 8/3/24.
 //
 
-import Foundation
+import EssentialNews
+
+public struct FeedArticleDetailsViewModel {
+    private let viewModel: ArticleDetailViewModel
+    
+    public init(viewModel: ArticleDetailViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var author: String {
+        viewModel.author ?? ""
+    }
+    
+    var title: String {
+        viewModel.title
+    }
+    
+    var description: String {
+        viewModel.description ?? ""
+    }
+        
+    var publishedDate: String {
+        convertDate(with: viewModel.publishedAt)
+    }
+    
+    var content: String {
+        viewModel.content ?? ""
+    }
+}
