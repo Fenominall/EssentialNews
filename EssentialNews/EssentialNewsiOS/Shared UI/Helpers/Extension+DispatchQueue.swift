8b0780ae2745eefba1f1c9f42d0220042e940a97
Vladislav Todorov <it.vtodorov@gmail.com>
2024-08-04 09:26:54 +0300

    Added a helper mainAsync method to the DispatchQueue to Dipatch on the work main thread

--- EssentialNews/EssentialNewsiOS/Shared UI/Helpers/Extension+DispatchQueue.swift
+++ EssentialNews/EssentialNewsiOS/Shared UI/Helpers/Extension+DispatchQueue.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+extension DispatchQueue {
+    static func mainAsync(execute work: @escaping () -> Void) {
+        if Thread.isMainThread {
+            work()
+        } else {
+            DispatchQueue.main.async(execute: work)
+        }
+    }
+}
+
